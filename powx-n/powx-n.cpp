917ba29f29d21cec885cc63c8ad68f5adc41248a
sumitanand027 <62176091+sumitanand027@users.noreply.github.com>
2021-12-31 01:48:16 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (10.87%) - LeetHub

---
+++ powx-n/powx-n.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0)
+            return 1;
+        if(n<0){
+            n = abs( n );
+            x = 1/x;
+        }
+        return (n%2 == 0) ? pow(x*x, n/2) : x*pow(x*x, n/2);
+    }
+};
