06ca06602e04b9bfbe65c3c801f6036445387812
sumitanand027 <62176091+sumitanand027@users.noreply.github.com>
2022-03-23 17:14:23 +0530

    Time: 38 ms (8.14%), Space: 20.3 MB (83.22%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -13,52 +13,24 @@ class Solution {
 public:
     
     
-    TreeNode * adjBST( TreeNode * cur )
-    {
-        if( cur -> right == NULL && cur -> left == NULL )
-            return NULL;
-        
-        else if( cur -> left && cur -> right == NULL )
-            return cur -> left;
-        
-        else if( cur -> left == NULL && cur -> right )
-            return cur -> right;
-        
-        TreeNode * it = cur -> right;
-        
-        while( it -> left != NULL ) {
-            it = it -> left;
-        } 
-        
-        it -> left = cur -> left;
-        return cur -> right;
-    }
-    
     TreeNode * dfs( TreeNode * cur , int l , int h )
     {
         if( cur == NULL )
             return NULL;
         
-//         if( cur -> val < l || cur -> val > h ) {
-//             // TreeNode * newCur = adjBST( cur );
-//             // if( newCur == NULL ) return NULL;
-            
-//             cur = newCur;
-//         }
-        
-//         if( cur -> val < l )
-//         {
-//             if( cur -> right )
-//                 cur = cur -> right;
-//             else 
-//                 return NULL;
-//         }
-//         if( cur -> val > h ) {
-//             if( cur -> left )
-//                 cur = cur ->left;
-//             else 
-//                 return NULL;
-//         }
+        if( cur -> val < l )
+        {
+            if( cur -> right )
+                cur = cur -> right;
+            else 
+                return NULL;
+        }
+        if( cur -> val > h ) {
+            if( cur -> left )
+                cur = cur ->left;
+            else 
+                return NULL;
+        }
         
         cur -> left = dfs( cur -> left , l , h );
         cur -> right = dfs( cur -> right , l , h );
