0c0a92d5c9aa24bbaff209d8bc7f0e9f5671588c
sumitanand027 <62176091+sumitanand027@users.noreply.github.com>
2022-04-11 19:11:11 +0530

    Time: 416 ms (12.76%), Space: 134.1 MB (5.39%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    unordered_set<int> safe;
+    unordered_set<int> cycle;
+    
+    bool dfs( int x , unordered_set<int> vis , vector< vector<int> > &graph )
+    {
+        if( safe.find( x ) != safe.end() )
+            return true;
+        if( cycle.find( x ) != cycle.end() )
+            return false;
+        
+        if( vis.find( x ) != vis.end() ) {
+            cycle.insert( x );
+            return false;
+        }
+        
+        vis.insert( x );
+        
+        for( auto i : graph[x] ) {
+            if( !dfs( i , vis , graph ) ) {
+                cycle.insert( x );
+                return false;
+            }
+        }
+        
+        safe.insert( x );
+        return true;
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
+        vector<int> ans;
+        
+        unordered_set<int> vis;
+        
+        for( int i = 0 ; i < graph.size() ; i++ )
+        {
+            if( dfs( i, vis , graph ) )
+                ans.push_back( i );
+        }
+        
+        return ans;
+    }
+};
