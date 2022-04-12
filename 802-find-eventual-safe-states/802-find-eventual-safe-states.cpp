13750b7d94aeaa9c58730327cb60e408a898f185
sumitanand027 <62176091+sumitanand027@users.noreply.github.com>
2022-04-12 16:28:35 +0530

    Time: 192 ms (64.78%), Space: 61.6 MB (19.66%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
-    
-    unordered_set<int> safe;
-    unordered_set<int> cycle;
-    
-    bool dfs( int x , unordered_set<int> &vis , vector< vector<int> > &graph )
-    {
-        if( safe.find( x ) != safe.end() )
-            return true;
-        if( cycle.find( x ) != cycle.end() )
-            return false;
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector< vector<int> > g(n);
+        vector<int>  outdegree( n, 0 );
         
-        if( vis.find( x ) != vis.end() ) {
-            cycle.insert( x );
-            return false;
-        }
-        
-        vis.insert( x );
-        
-        for( auto i : graph[x] ) {
-            if( !dfs( i , vis , graph ) ) {
-                cycle.insert( x );
-                return false;
+        for( int i = 0 ; i < n ; i++ ) {
+            for( auto j : graph[i]) {
+                g[j].push_back( i );
+                outdegree[i]++;
             }
         }
         
-        safe.insert( x );
-        return true;
-    }
-    
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        queue<int> q;
+        
+        for( int i = 0 ; i < n ; i++ ) 
+        {
+            if( outdegree[i] == 0 )
+                q.push( i );        
+        }
         
         vector<int> ans;
         
-        unordered_set<int> vis;
-        
-        for( int i = 0 ; i < graph.size() ; i++ )
+        while( !q.empty() )
         {
-            if( dfs( i, vis , graph ) )
-                ans.push_back( i );
+            int k = q.front();
+            q.pop();
+ 
+            ans.push_back( k );
+            for( auto i : g[k] ) {
+                outdegree[i]--;
+                if( outdegree[i] == 0 ) {
+                    q.push( i );
+                }
+            }
+            
         }
         
-        return ans;
+        sort( ans.begin() , ans.end() );
+        return ans;        
     }
 };
