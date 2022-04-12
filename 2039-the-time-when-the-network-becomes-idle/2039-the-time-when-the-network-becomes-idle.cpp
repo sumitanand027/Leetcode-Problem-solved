a49f9c34703bb3e34615cdf2d796fafe73d947e1
sumitanand027 <62176091+sumitanand027@users.noreply.github.com>
2022-04-12 20:28:46 +0530

    Time: 1390 ms (10.00%), Space: 220 MB (11.21%) - LeetHub

---
+++ 2039-the-time-when-the-network-becomes-idle/2039-the-time-when-the-network-becomes-idle.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
+        
+        unordered_map<int,vector<int> > adj;
+        
+        
+        for( int i = 0 ; i < edges.size() ; i++ ) {
+            adj[ edges[i][0] ].push_back( edges[i][1] );
+            adj[ edges[i][1] ].push_back( edges[i][0] );
+        }
+        
+        int n = adj.size();
+        vector<int> dist( n , INT_MAX );
+        vector<int> vis(n,0);
+        
+        queue<int> q;
+        q.push( 0 );
+        vis[0] = 1;
+        dist[0] = 0;
+        
+    
+        while ( !q.empty() )
+        {
+            int k = q.front();
+            q.pop();
+            
+            for( auto it : adj[k] )
+            {
+                if( vis[it] == 0 ) {
+                    q.push( it );
+                    vis[ it ] = 1;
+                    dist[it] = dist[k] + 1; 
+                } else {
+                    dist[it] = min( dist[it] , dist[k] + 1 );
+                }
+            }
+        }
+        
+        
+        int maxVal = 0;
+        for( int i = 0 ; i < n; i++ )
+        {
+            cout << dist[i] << endl;
+            
+            maxVal = max( maxVal , dist[i] );
+        }
+        
+        
+        int slowest = 0;
+        for (int i = 1; i < n; i++) {
+            int firstTimeReceive = dist[i] * 2;
+            int waitingTime = firstTimeReceive - 1;
+            int numResend = waitingTime / patience[i];
+            int timeDone = firstTimeReceive + numResend * patience[i];
+            slowest = max(slowest, timeDone);
+        }
+        return slowest + 1;
+        
+    }
+    
+    
+};
