443f5c5c9f47c454b4ebb9fc07a4c24d81462b35
sumitanand027 <62176091+sumitanand027@users.noreply.github.com>
2022-05-28 15:47:31 +0530

    Time: 20 ms (50.92%), Space: 13.3 MB (54.51%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        int presentInX = 0 , presentInY = 0;
+        
+        for( int i = 0 ; i < matrix.size() ; i++ )
+            if( matrix[i][0] == 0 )
+                presentInX = 1;
+        
+        for( int j = 0 ; j < matrix[0].size() ; j++ )
+            if( matrix[0][j] == 0 )
+                presentInY = 1;
+        
+        cout << presentInX << " " << presentInY << endl;
+        
+        for( int i = 1 ; i < matrix.size() ; i++ )
+        {
+            for( int j = 1 ; j < matrix[0].size() ; j++ )
+            {
+                if( matrix[i][j] == 0 )
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for( int i = matrix.size() - 1 ; i >= 0 ; i-- )
+        {
+            for( int j = matrix[0].size() - 1 ; j >= 0 ; j-- )
+            {
+                if( matrix[i][0] == 0 || matrix[0][j] == 0 ) {
+                    matrix[i][j] = 0;
+                } 
+            }
+        }
+        
+        for( int i = 0 ; i < matrix.size() && presentInX ; i++ )
+            matrix[i][0] = 0;
+        
+         for( int i = 0 ; i < matrix[0].size() ; i++ )
+            if( presentInY )
+                matrix[0][i] = 0;
+    }
+};
