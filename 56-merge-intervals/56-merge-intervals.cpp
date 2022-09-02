5ed0efd9f03acb4745dc0d5252a7c0e9fa97dac0
sumitanand027 <62176091+sumitanand027@users.noreply.github.com>
2022-09-02 10:11:03 +0530

    Time: 83 ms (21.79%), Space: 19 MB (67.18%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -2,16 +2,22 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& inte) {
         
-        sort( inte.begin() , inte.end() );
+        sort( inte.begin(), inte.end() );
+        int lastS = inte[0][0], lastE = inte[0][1];
+        
         vector< vector<int> > ans;
         
-        for( auto &i : inte ) {
-            if( ans.size() > 0 && ans[ ans.size() - 1 ][1] >= i[0] ) {
-                ans[ ans.size() - 1 ][1] = max( i[1] , ans.back()[1]  );
+        for( int i = 1; i < inte.size(); i++ )
+        {
+            if( inte[i][0] > lastE ) {
+                ans.push_back( {lastS , lastE} );
+                lastS = inte[i][0];
+                lastE = inte[i][1];
             } else {
-                ans.push_back( {i[0] , i[1] });
+                lastE = max( lastE , inte[i][1] );
             }
         }
+        ans.push_back({lastS , lastE} );
         return ans;
     }
 };
